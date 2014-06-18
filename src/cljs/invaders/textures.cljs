768f10701b08f7301062fc1761d653d07f3178bd
keymone <keymone@gmail.com>
2014-06-18 21:57:34 +0200

    Add proper hit areas for tiles

--- src/cljs/invaders/textures.cljs
+++ src/cljs/invaders/textures.cljs
@@ -9,9 +9,10 @@
                 :s (create-texture "/images/sand.png")
                 :marsman (create-texture "/images/marsman.png") })
 
+; hit-area coordinates are top-down, left-right
 (def hit-areas {
-                 :w nil
-                 :g nil
-                 :s nil
-                 :marsman (array 25 3 25 44 20 54 19 69 31 83 49 83 60 69 60 55
-                                 55 47 54 21 48 1)})
+                 :w (array 0 26 0 66 40 83 80 66 80 26 40 8)
+                 :g (array 0 17 0 66 40 83 80 66 80 17 40 0)
+                 :s (array 0 17 0 66 40 83 80 66 80 17 40 0)
+                 :marsman (array 33 0 20 15 20 28 25 38 25 67 38 83 51 77 54 62
+                                 55 37 60 28 60 15 47 0)})
